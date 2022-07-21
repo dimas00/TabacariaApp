9fdfa95b2a9e8c3f47b688e6475bb07f5be98fac
dimas <dimasfernandes00@gmail.com>
2022-07-21 15:34:24 -0300

    rotas direcionadas

--- src/main/java/br/csi/controller/IndexController.java
+++ src/main/java/br/csi/controller/IndexController.java
@@ -1,17 +1,33 @@
 package br.csi.controller;
 
+import br.csi.dao.ProdutoDao;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
-@RequestMapping("/")
 public class IndexController {
+
+    ProdutoDao pDao = new ProdutoDao();
 
     @GetMapping("/login")
     public String login(){
         return "login" ;
 
+    }
+
+    @GetMapping("/produtos")
+    public String produtos(Model model) {
+        model.addAttribute("produtos", pDao.getProdutos());
+
+        return "visualizarproduto";
+    }
+
+    @GetMapping("/")
+    public RedirectView indexController() {
+        return new RedirectView("/produtos", true);
     }
 
 }
